1810808e97be0daffd7c406a2b7a3840b1bcb41e
Liviu Maria <marialiviuvalentin@gmail.com>
2017-01-28 23:04:58 +0200

    added sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :load_chat_rooms
+
+  private
+
+  def load_chat_rooms
+    @chat_rooms = ChatRoom.where(user: current_user)
+  end
 end
